e46519c23abb36d04e061ebcd6a88bf2127e12e4
ErickDev1218 <erickpdm879@gmail.com>
2024-09-05 14:55:32 -0300

    Att: String can be translated to a tree.

---
+++ src/main.ts
@@ -0,0 +1,104 @@
+enum OPERATOR {
+    SUM,// SOMA
+    SUB,// SUBTRACAO
+    DIV,// DIVISAO
+    MUL,// MULTIPLICACAO
+    MEM,// LOAD
+    MEV, // MOVEM
+    MOV,// STORE
+    OPP,// (
+    CPP // )
+}
+
+const operatorsMap: Map<string, OPERATOR> = new Map<string, OPERATOR>([
+    ["+", OPERATOR.SUM],
+    ["-", OPERATOR.SUB],
+    ["/", OPERATOR.DIV],
+    ["*", OPERATOR.MUL],
+    ["MEM", OPERATOR.MEM],
+    ["MOV", OPERATOR.MOV],
+    ["(", OPERATOR.OPP],
+    [")", OPERATOR.CPP]
+])
+
+// Fica subetendido que o que nao é operador é operando.
+
+type TreeNode = {
+    root: string
+    childLeft?: TreeNode 
+    childRight?: TreeNode
+} | null
+
+function splitLinearString(str: string) : string[] {
+    const chars : string[] = str.split("")
+    const ret : string[] = [];
+
+    for(let i = 0; i < chars.length; i++) {
+        if(operatorsMap.has(chars[i])) {
+            ret.push(chars[i]) 
+        } else {
+            let full_str = ""
+            while(i < chars.length-1 && ![",", "(", ")"].includes(chars[i+1])) {
+                if(chars[i] !== ","){
+                    full_str += chars[i]
+                }
+                i++;
+            }
+            full_str += chars[i]
+            ret.push(full_str)
+        }
+    }
+
+    return ret
+}
+
+function linearStringToTree(str: string): TreeNode {
+    const tokens = splitLinearString(str);
+    const stack : string[] = []
+    const queue : TreeNode[] = []
+    const ret : TreeNode = {
+        root: null,
+        childLeft: null,
+        childRight: null
+    }
+
+    console.log(tokens)
+
+    for(const token of tokens) {
+        if(operatorsMap.has(token) && operatorsMap.get(token) === OPERATOR.CPP) {
+            while(stack.length !== 0 && operatorsMap.get(stack[stack.length-1]) !== OPERATOR.OPP) {
+                const curr = stack.pop()
+                console.log(curr)
+                if(!operatorsMap.has(curr)) {
+                    queue.unshift({
+                        root: curr,
+                        childLeft: null,
+                        childRight: null
+                    })
+                } else {
+                    const left = queue.length >= 1 ? queue.shift() : null;
+                    const right = queue.length >= 1 ? queue.shift() : null;
+                    queue.unshift({
+                        root: curr,
+                        childLeft: left,
+                        childRight: right
+                    })
+                }
+            }
+            stack.pop()
+        } else {
+            stack.push(token)
+        }
+    }
+
+
+    return queue.shift() 
+}
+
+function main() {
+    let x = '(MEM(+(CONST1,CONST2)))'
+
+    console.log(linearStringToTree(x))
+}
+
+main()
