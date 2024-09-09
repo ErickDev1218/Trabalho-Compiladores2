9ebd569834f4b3ce3ff4311f794829352242da35
ErickDev1218 <erickpdm879@gmail.com>
2024-09-09 16:51:51 -0300

    Tentativa de solucao da Q1

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,20 @@ enum OPERATOR {
   MEM, // LOAD
   MEV, // MOVEM
   MOV, // STORE
-  OPP, // (
-  CPP, // )
+  // OPP, // (
+  // CPP, // )
 }
 
-const operatorsMap: Map<string, OPERATOR> = new Map<string, OPERATOR>([
-  ["+", OPERATOR.SUM],
-  ["-", OPERATOR.SUB],
-  ["/", OPERATOR.DIV],
-  ["*", OPERATOR.MUL],
-  ["MEM", OPERATOR.MEM],
-  ["MOV", OPERATOR.MOV],
-  ["(", OPERATOR.OPP],
-  [")", OPERATOR.CPP],
+const operatorsMap: Map<string, [OPERATOR, number]> = new Map<string, [OPERATOR, number]>([
+  ["+", [OPERATOR.SUM, 2]],
+  ["-", [OPERATOR.SUB, 2]],
+  ["/", [OPERATOR.DIV, 2]],
+  ["*", [OPERATOR.MUL, 2]],
+  ["MEV", [OPERATOR.MEV, 2]],
+  ["MEM", [OPERATOR.MEM, 1]],
+  ["MOV", [OPERATOR.MOV, 2]],
+  // ["(", [ OPERATOR.OPP, 0]],
+  // [")", [ OPERATOR.CPP, 0]],
 ]);
 
 type TreeNode = {
@@ -49,7 +50,7 @@ function splitLinearString(str: string): string[] {
       if (chars[i] !== ",") {
         full_str += chars[i];
       }
-      if(full_str !== ""){
+      if (full_str !== "") {
         ret.push(full_str);
       }
     }
@@ -58,63 +59,54 @@ function splitLinearString(str: string): string[] {
   return ret;
 }
 
-function linearStringToTree(str: string): TreeNode {
-  if (!str.startsWith("(")) {
-    str = "(" + str;
-  }
-  if (!str.endsWith("(")) {
-    str = str + ")";
-  }
-  const tokens = splitLinearString(str);
-  const stack: string[] = [];
-  const queue: TreeNode[] = [];
-  const ret: TreeNode = {
-    root: null,
-    childLeft: null,
-    childRight: null,
-  };
+// function linearStringToTree(str: string): TreeNode {
+//   const tokens = splitLinearString(str);
+//   console.log(tokens)
+//   const stack: string[] = [];
+//   const queue: TreeNode[] = [];
+//   const ret: TreeNode = {
+//     root: null,
+//     childLeft: null,
+//     childRight: null,
+//   };
 
-  for (const token of tokens) {
-    if (operatorsMap.has(token) && operatorsMap.get(token) === OPERATOR.CPP) {
-      while (
-        stack.length !== 0 &&
-        operatorsMap.get(stack[stack.length - 1]) !== OPERATOR.OPP
-      ) {
-        const curr = stack.pop();
-        if (!operatorsMap.has(curr)) {
-          queue.unshift({
-            root: curr,
-            childLeft: null,
-            childRight: null,
-          });
-        } else {
-          const left = queue.length >= 1 ? queue.shift() : null;
-          const right = queue.length >= 1 ? queue.shift() : null;
-          queue.unshift({
-            root: curr,
-            childLeft: left,
-            childRight: right,
-          });
-        }
-      }
-      if (stack.length !== 0) {
-        stack.pop();
-      } else {
-        throw new Error(
-          `ERROR :: linearStringToTree :: Malformed Formula -> ${str}`
-        );
-      }
-    } else {
-      stack.push(token);
-    }
-  }
-  if (stack.length !== 0) {
-    throw new Error(
-      `ERROR :: linearStringToTree :: Malformed Formula -> ${str}`
-    );
-  }
-  return queue.shift();
-}
+//   for (const token of tokens) {
+//     if (operatorsMap.has(token)) {
+//       if (operatorsMap.get(token) === OPERATOR.CPP) {
+//         while (
+//           stack.length !== 0 &&
+//           operatorsMap.get(stack[stack.length - 1]) !== OPERATOR.OPP
+//         ) {
+//           const op = stack.pop();
+//           const left = queue.length >= 1 ? queue.shift() : null;
+//           const right = queue.length >= 1 && op !== "MEM" ? queue.shift() : null;
+//           queue.unshift({
+//             root: op,
+//             childLeft: left,
+//             childRight: right,
+//           });
+//         }
+//         if (stack.length !== 0) {
+//           stack.pop();
+//         } else {
+//           throw new Error(
+//             `ERROR :: linearStringToTree :: Malformed Formula -> ${str}`
+//           );
+//         }
+//       } else {
+//         stack.push(token)
+//       }
+//     } else {
+//       queue.unshift({
+//         root: token,
+//         childLeft: null,
+//         childRight: null
+//       })
+//     }
+//   } 
+
+//   return queue.shift();
+// }
 
 async function generateGraphInBase64(graph): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -130,34 +122,34 @@ async function generateGraphInBase64(graph): Promise<string> {
   });
 }
 
-async function ParseTreeToGraphvizB64(tree : TreeNode) {
+async function ParseTreeToGraphvizB64(tree: TreeNode) {
   let globalId = 0
-  const stack : TreeNode[] = []
-  const idStack : number[] = []
+  const stack: TreeNode[] = []
+  const idStack: number[] = []
   let graphvizStr = `graph {
 node[shape="box"]`
   stack.push(tree)
   idStack.push(globalId)
 
-  while(stack.length !== 0) {
+  while (stack.length !== 0) {
     const currNode = stack.pop()
     const currId = idStack.pop()
     graphvizStr += `\n${currId}[label="${currNode.root}"]\n`
 
-    if(currNode.childLeft !== null) {
-      globalId++
-      stack.push(currNode.childLeft)
-      idStack.push(globalId);
-      graphvizStr += `${currId}--${globalId}\n`
-    }
-    if(currNode.childRight !== null) {
+    if (currNode.childRight !== null) {
       globalId++
       stack.push(currNode.childRight)
       idStack.push(globalId);
       graphvizStr += `${currId}--${globalId}\n`
     }
+    if (currNode.childLeft !== null) {
+      globalId++
+      stack.push(currNode.childLeft)
+      idStack.push(globalId);
+      graphvizStr += `${currId}--${globalId}\n`
+    }
   }
-  
+
   graphvizStr += "\n}"
 
   const svg = await graphviz.dot(graphvizStr, 'svg')
@@ -167,6 +159,86 @@ node[shape="box"]`
 
 export async function generateLinearStringB64(linearString) {
   const tree = linearStringToTree(linearString)
+  console.log(tree)
   const ret = await ParseTreeToGraphvizB64(tree)
   return ret;
+}
+
+
+//Testando com recursão
+
+function rec(str: string) {
+  if (str.length === 0) {
+    return null
+  }
+  let _str = ''
+  let i = 0
+  while (!str.includes('(')) {
+    _str = str[i]
+  }
+  _str = _str.substring(0, _str.length - 1)
+  const final: TreeNode = {
+    root: _str,
+    childLeft: rec(_str),
+    childRight: rec(_str) //depois de remover até o proximo )
+  }
+}
+
+function linearStringToTree(str: string): TreeNode {
+  str = str.trim()
+  const root = str.substring(0, str.indexOf("(") === -1 ? str.length : str.indexOf("("))
+  console.log("str:", str)
+  console.log("root:", root)
+  if (!operatorsMap.has(root)) {
+    return {
+      root,
+      childLeft: null,
+      childRight: null
+    }
+  } else {
+    let flag = false;
+    let counter = 0;
+    let index = 0;
+    let left;
+    let right;
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "(") {
+        counter++;
+        if (!flag) {
+          flag = true
+        }
+      }
+      if (str[i] === ")") {
+        counter--;
+        if (counter === 1) {
+          index = i;
+          break;
+        }
+      }
+    }
+
+    if (counter === 0) {
+      right = str.substring(str.indexOf("(") + 1, str.length - 1).split(",")[1]
+      left = str.substring(str.indexOf("(") + 1, str.length - 1).split(",")[0]
+    } else {
+      left = (str.substring(str.indexOf("(") + 1, index + 1))
+      let temp = str.substring(index + 1, str.length)
+      right = temp.substring(temp.indexOf(",") + 1, temp.length - 1)
+    }
+
+    if (operatorsMap.get(root)[1] === 1) {
+      return {
+        root,
+        childLeft: linearStringToTree(left),
+        childRight: null
+      }
+    } else if (operatorsMap.get(root)[1] === 2) {
+      return {
+        root,
+        childLeft: linearStringToTree(right),
+        childRight: linearStringToTree(left)
+      }
+    }
+  }
 }
