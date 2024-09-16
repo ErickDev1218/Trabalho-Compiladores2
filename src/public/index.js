2619c46ddc9986bfee731527f851944a184cac54
ErickDev1218 <erickpdm879@gmail.com>
2024-09-16 12:19:08 -0300

    att: changing the css for index.html

--- src/public/index.js
+++ src/public/index.js
@@ -3,6 +3,8 @@ const btn = document.querySelector("#btn");
 const input = document.querySelector("#input");
 const costDiv = document.querySelector("#cost-div")
 const codeCard = document.querySelector("#code-card");
+const showDiv = document.querySelector('#dinamic')
+const badFormat = document.querySelector('#badFormat')
 
 btn.addEventListener("click", async () => {
   try {
@@ -25,10 +27,23 @@ btn.addEventListener("click", async () => {
 
     const data = await response.json();
     // console.log("Success:", data);
+    showDiv.classList.add('generateContainer')
+    img.style.display = 'block'
     img.src = `data:image/svg+xml;base64,${data.b64Tree}`
     costDiv.innerText = `Custo: ${data.costTree}`
-    codeCard.innerText = data.codeTree.join("\n")
+    codeCard.innerHTML = ''
+    data.codeTree.forEach(ea => {
+      codeCard.innerHTML += `<li> ${ea} </li>`
+    })
+    badFormat.innerText = ''
+    // codeCard.innerText = data.codeTree.join("\n")
   } catch (error) {
+    codeCard.innerHTML = ''
+    img.src = ''
+    img.style.display = 'none'
+    costDiv.innerHTML = ''
+    badFormat.innerText = 'Entrada fora do formato esperado!'
+    showDiv.classList.remove('generateContainer')
     console.error("Error:", error);
   }
 });
